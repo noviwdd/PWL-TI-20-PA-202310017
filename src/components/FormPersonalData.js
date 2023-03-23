494d2e671b91db800e52f1c655767e957c1b7de5
Novi Widia Damayanti <noviwdd2@gmail.com>
2023-03-24 02:17:56 +0700

    P02 - Change to Multiple Form

--- src/components/FormPersonalData.js
+++ src/components/FormPersonalData.js
@@ -1,112 +1,233 @@
-import React, { useState } from 'react'
-import { Button, Container, Form, Row, Col, Modal } from "react-bootstrap"
+import React, { useState } from "react";
+import { Container, Modal, Form, Table, ButtonGroup, Button } from "react-bootstrap";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const FormPersonalData = () => {
-    const date = new Date();
-    const currentYear = date.getFullYear();
-
-    const [NPM, setNPM] = useState(0);
-    const [firstName, setFirstName] = useState("");
-    const [middleName, setMiddleName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [birthdate, setBirthdate] = useState("");
-    const [show, setShow] = useState(false);
-    const [validated, setValidated] = useState(false);
-
-    const handleClose = () => setShow(false);
-
-    const handleSubmit = (e) => {
-        const form = e.currentTarget;
-        if(form.checkValidity() === false) {
-            e.preventDefault();
-            e.stopPropagation();
-        } else {
-            e.preventDefault();
-            setShow(true);
-        }
-        setValidated(true);
+    var today = new Date();
+    const currDate = formatDate(today);
+    const objProfile = {
+        npm: 0,
+        fname: "",
+        mname: "",
+        lname: "",
+        birthdate: currDate,
     };
 
-    const age = currentYear - parseInt(birthdate.slice(0,4));
+    const [profile, setProfile] = useState([objProfile]);
+    const [mymodal, setMymodal] = useState({
+        show: false,
+        message: "",
+    });
+
+    const RowForm = ({ data, index }) => {
+        return (
+            <tr>
+                <td>
+                    <Form.Control
+                        placeholder="NPM"
+                        maxLength="9"
+                        pattern="^[0-9]*$"
+                        defaultValue={data.npm ? data.npm : ''}
+                        onChange={(e) =>
+                            setProfile((prev) => {
+                                const newdata = [...prev];
+                                newdata[index].npm = parseInt(e.target.value);
+                                return newdata;
+                            })}
+                        required
+                    />
+                </td>
+                <td>
+                    <Form.Control
+                        type="text"
+                        placeholder="First Name"
+                        defaultValue={data.fname}
+                        onChange={(e) =>
+                            setProfile((prev) => {
+                                const newdata = [...prev];
+                                newdata[index].fname = e.target.value;
+                                return newdata;
+                            })}
+                        required
+                    />
+                </td>
+                <td>
+                    <Form.Control
+                        type="text"
+                        placeholder="Middle Name"
+                        defaultValue={data.mname}
+                        onChange={(e) =>
+                            setProfile((prev) => {
+                                const newdata = [...prev];
+                                newdata[index].mname = e.target.value;
+                                return newdata;
+                            })}
+                        required
+                    />
+                </td>
+                <td>
+                    <Form.Control
+                        type="text"
+                        placeholder="Last Name"
+                        defaultValue={data.lname}
+                        onChange={(e) =>
+                            setProfile((prev) => {
+                                const newdata = [...prev];
+                                newdata[index].lname = e.target.value;
+                                return newdata;
+                            })}
+                        required
+                    />
+                </td>
+                <td>
+                    <DatePicker
+                        selected={new Date(data.birthdate)}
+                        onChange={(value) =>
+                            setProfile((prev) => {
+                                const newdata = [...prev];
+                                newdata[index].birthdate = formatDate(value);
+                                return newdata;
+                            })
+                        }
+                        className="form-control datepicker"
+                        placeholder="Choose Date"
+                        required
+                    />
+                </td>
+                {index ? (
+                    <td>
+                        <Button
+                            variant="danger"
+                            type="button"
+                            onClick={(e) =>
+                                setProfile((prev) => {
+                                    const newdata = [...prev];
+                                    delete newdata[index];
+                                    return newdata;
+                                })
+                            }
+                        >
+                            Remove
+                        </Button>
+                    </td>
+                ) : (
+                    <td></td>
+                )
+
+                }
+            </tr>
+        );
+    };
+
+    const result = (data) => {
+        return (
+            <div>
+                {data.map((v, index) =>
+                    v ? (
+                        <div className="profile border-bottom" key={index}>
+                            <p className="text-dark">NPM: {v.npm}</p>
+                            <p className="text-dark">Fullname: {v.fname} {v.mname} {v.lname} </p>
+                            <p className="text-dark">Birthdate: {v.birthdate}</p>
+                            <p className="text-dark">Age: {calculateAge(v.birthdate)} years old</p>
+                        </div>
+                    ) : (
+                        ''
+                    )
+                )}
+            </div>
+        );
+    };
+
+    const handlerSubmit = (e) => {
+        e.preventDefault();
+        let myresult = result(profile);
+        setMymodal({ ...mymodal, show: true, message: myresult });
+    };
+
+    const modalHandleClose = () => {
+        setMymodal({ ...mymodal, show: false });
+    };
+
 
     return (
-        <Container>
-            <div className="d-flex align-items-center justify-content-center my-5">
-                <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                    <Row className="mb-3">
-                        <Form.Group as={Col} className="mb-3" controlId="formNPM">
-                            <Form.Control  
-                                placeholder="NPM"
-                                maxLength="9"
-                                onChange={(e) => setNPM(e.target.value)}
-                                onKeyPress={(e) => {if (!/[0-9]/.test(e.key)) e.preventDefault();}} 
-                                required />
-                        </Form.Group>
+        <Container fluid>
+            <div className="my-5">
+                <Form onSubmit={(e) => handlerSubmit(e)} id="form-profile">
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>NPM</th>
+                                <th>First Name</th>
+                                <th>Middle Name</th>
+                                <th>Last Name</th>
+                                <th>Birthdate</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {profile.map((v, index) =>
+                                v ? <RowForm key={index} data={v} index={index} /> : ""
+                            )}
 
-                        <Form.Group className="mb-3" controlId="formFirstName">
-                            <Form.Control 
-                                type="text" 
-                                placeholder="First Name"
-                                onChange={(e) => setFirstName(e.target.value)} />
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="formMiddleName">
-                            <Form.Control 
-                                type="text" 
-                                placeholder="Middle Name"
-                                onChange={(e) => setMiddleName(e.target.value)} />
-                        </Form.Group>
-
-                        <Form.Group as={Col} controlId="formLastName">
-                            <Form.Control 
-                                type="text" 
-                                placeholder="Last Name"
-                                onChange={(e) => setLastName(e.target.value)} />
-                        </Form.Group>
-                    </Row>
-                    <Form.Group className="mb-3" controlId="formBirthdate">
-                        <input
-                            placeholder="YYYY-MM-DD"
-                            type="text"
-                            class="form-control datepicker"
-                            name="Birthdate"
-                            onChange={(e) => setBirthdate(e.target.value)}
-                            required />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
+                        </tbody>
+                    </Table>
+                    <Button
+                        variant="success"
+                        type="button"
+                        onClick={() =>
+                            setProfile((prev) => {
+                                return [...prev, objProfile];
+                            })
+                        }>
+                        Add new row
                     </Button>
-                </Form>
 
-                <Modal show={show} onHide={handleClose} centered>
-                    <Modal.Header closeButton className="d-flex justify-content-center">
-                        <Modal.Title>Form Mahasiswa</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Container>
-                            <Row>
-                            <Col xs="3">NPM</Col>
-                            <Col xs="1">:</Col>
-                            <Col>{NPM}</Col>
-                            </Row>
-                            <Row>
-                            <Col xs="3">Fullname</Col>
-                            <Col xs="1">:</Col>
-                            <Col>
-                                {firstName} {middleName} {lastName}
-                            </Col>
-                            </Row>
-                            <Row>
-                            <Col xs="3">Age</Col>
-                            <Col xs="1">:</Col>
-                            <Col>{age}{" years old"}</Col>
-                            </Row>
-                        </Container>
-                    </Modal.Body>
-                </Modal>
+                    <div className="float-end">
+                        <ButtonGroup className="mb-2">
+                            <Button variant="light" type="reset">Clear</Button>
+                            <Button variant="primary" type="submit" className="btn-submit">Submit</Button>
+                        </ButtonGroup>
+                    </div>
+                </Form>
             </div>
+            <ModalPopUP show={mymodal.show} handleClose={modalHandleClose} message={mymodal.message}/>
         </Container>
-    )
+    );
 }
+
+const formatDate = (datestring) => {
+    const today = new Date(datestring);
+    const yyyy = today.getFullYear();
+    let mm = today.getMonth() + 1; // Months start at 0!
+    let dd = today.getDate();
+
+    if (dd < 10) dd = "0" + dd;
+    if (mm < 10) mm = "0" + mm;
+
+    return yyyy + "-" + mm + "-" + dd;
+};
+
+const calculateAge = (birthdate) => {
+    var today = new Date();
+    var birthDate = new Date(birthdate);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    return age;
+};
+
+const ModalPopUP = ({ show, handleClose, message }) => {
+    return (
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Information</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{message}</Modal.Body>
+        </Modal>
+    );
+};
 
 export default FormPersonalData;
